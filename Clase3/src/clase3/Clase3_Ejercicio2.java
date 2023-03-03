174cedf21f7d7d3cdae4d1bdbd6239c79a4d8627
MartyX265 <116092154+MartyX265@users.noreply.github.com>
2023-03-03 11:17:18 -0300

    Update Clase3_Ejercicio2.java

--- Clase3/src/clase3/Clase3_Ejercicio2.java
+++ Clase3/src/clase3/Clase3_Ejercicio2.java
@@ -4,13 +4,13 @@ package clase3;
 public class Clase3_Ejercicio2 {
     public static String CodificarMas1(String texto){
         String resultado="";
-        String diccionario="abcdefghijklmnñopqrstuvwxyz@";
+        String diccionario="abcdefghijklmnñopqrstuvwxyz0";
         for(int i=0;i<texto.length();i++){
             if(texto.charAt(i)=='z'){
-               resultado+=diccionario.charAt(0);
+               resultado+=diccionario.charAt(diccionario.length()-1);
             }else{
                 if(texto.charAt(i)==' '){
-                    resultado+=diccionario.charAt(diccionario.length()-1);
+                    resultado+=diccionario.charAt(0);
                 }else{
                   resultado+=diccionario.charAt(diccionario.indexOf(texto.charAt(i))+1);  
                 }
@@ -18,7 +18,24 @@ public class Clase3_Ejercicio2 {
         }
         return resultado;
     }
+    public static String DecodificarMenos1(String texto){
+        String resultado="";
+        String diccionario="abcdefghijklmnñopqrstuvwxyz0";
+        for(int i=0;i<texto.length();i++){
+            if(texto.charAt(i)=='0'){
+               resultado+=diccionario.charAt(diccionario.length()-2);
+            }else{
+                if(texto.charAt(i)=='a'){
+                    resultado+=' ';
+                }else{
+                  resultado+=diccionario.charAt(diccionario.indexOf(texto.charAt(i))-1);  
+                }
+            }
+        }
+        return resultado;
+    }
     public static void main(String[] args){
         System.out.println(CodificarMas1("hola que tal"));
+        System.out.println(DecodificarMenos1("ipmbarvfaubm"));
     }
 }
