82ecdaee178253709586c0fa1a6997dbbf346d66
MartyX265 <116092154+MartyX265@users.noreply.github.com>
2023-03-04 11:59:53 -0300

    Update Clase3_Ejercicio1A.java

--- Clase3/src/clase3/Clase3_Ejercicio1A.java
+++ Clase3/src/clase3/Clase3_Ejercicio1A.java
@@ -1,7 +1,21 @@
 
 package clase3;
-
+import java.util.Scanner;
 
 public class Clase3_Ejercicio1A {
-    
+    public static void main(String[] args){
+        Scanner entrada=new Scanner(System.in);
+        System.out.println("Ingrese un texto");
+        String texto=entrada.nextLine();
+        System.out.println("Ingrese una letra a buscar");
+        char letra=entrada.next().charAt(0);
+        int contador=0;
+        char[] array=texto.toCharArray();
+        for(char l:array){
+            if(l==letra){
+                contador++;
+            }
+        }
+        System.out.println(contador);
+    }
 }
