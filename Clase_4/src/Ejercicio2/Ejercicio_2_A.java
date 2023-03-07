aaeed05d8d553074addd1da71c42e6ef4be62bcb
MartyX265 <116092154+MartyX265@users.noreply.github.com>
2023-03-07 11:27:26 -0300

    Añadir Ejercicio 2

--- Clase_4/src/Ejercicio2/Ejercicio_2_A.java
+++ Clase_4/src/Ejercicio2/Ejercicio_2_A.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Ejercicio2;
 
-/**
- *
- * @author marti
+package Ejercicio2;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+/*
+ Haga una main donde por parámetro envíe la ruta de un archivo. Ese archivo debe
+contener números. El programa debe escribir por consola la suma de esos números
  */
 public class Ejercicio_2_A {
-    
+    public static int SumarDesdeArchivo(String ruta){
+        int sumatoria=0;
+        try{
+            Path rutaArchivo=Paths.get(ruta);
+            for(String datos:Files.readAllLines(rutaArchivo)){
+                sumatoria+=Integer.parseInt(datos);
+            }
+        }catch(IOException e){
+            System.out.println(e);
+            System.out.println("Hubo un error al leer el archivo");
+            return sumatoria;
+        }
+       return sumatoria;
+    }
+    public static void main(String[] args){
+        String ruta="D:\\Programacion\\Argentina Programa\\Argentina-Programa\\Clase_4\\numeros.txt";
+        System.out.println("El resultado es: "+SumarDesdeArchivo(ruta));
+    }
 }
---
+++ Clase_4/src/Ejercicio2/Ejercicio_2_B.java
@@ -0,0 +1,52 @@
+
+package Ejercicio2;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+/*
+ */
+public class Ejercicio_2_B {
+    public static int OperarDesdeArchivo(String ruta,String operacion){
+        int resultado;
+        if(operacion.equals("S")){
+            resultado=0;
+        }else{
+            resultado=1;
+        }
+        try{
+            Path rutaArchivo=Paths.get(ruta);
+            for(String datos:Files.readAllLines(rutaArchivo)){
+                switch(operacion){
+                    case "S"->{
+                        resultado+=Integer.parseInt(datos);
+                    }
+                    case "M"->{
+                        resultado*=Integer.parseInt(datos);
+                    }
+                }
+            }
+        }catch(IOException e){
+            System.out.println(e);
+            System.out.println("Hubo un error al leer el archivo");
+            return 0;
+        }
+       return resultado;
+    }
+    public static void main(String[] args){
+        String opcion,ruta="D:\\Programacion\\Argentina Programa\\Argentina-Programa\\Clase_4\\numeros.txt";
+        Scanner entrada=new Scanner(System.in);
+        do{
+            System.out.println("Para hacer una sumatoria ingrese (S) y para una multiplicacion ingrese (M)");
+            opcion=entrada.next();
+            if(opcion.equals("S") || opcion.equals("M")){
+                entrada.close();
+                break;
+            }
+            System.out.println("Opcion ingresada no es valida");
+        }while(true);
+        System.out.println("El resultado de la operacion es: "+OperarDesdeArchivo(ruta,opcion));
+    }
+}
